82db21d0898ffab1c609c16142806a2834bc4dc8
Hendika Lukman <hendika.lukman@commsult.id>
2026-02-12 18:04:35 +0700

    minor config change

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import yaml from '@rollup/plugin-yaml'
 
 export default defineConfig({
+  base: '/text_based_engine/',
   plugins: [
     svelte(),
     yaml()
